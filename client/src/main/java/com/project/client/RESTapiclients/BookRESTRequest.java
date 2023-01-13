ed653bfb45dc7f108ff29b53d53cacdba7d46772
Tom Lawton <vnminhduy0750@outlook.com>
2023-01-13 20:24:38 +0700

    Add 2 sample REST requests

--- client/src/main/java/com/project/client/RESTapiclients/BookRESTRequest.java
+++ client/src/main/java/com/project/client/RESTapiclients/BookRESTRequest.java
@@ -14,42 +14,44 @@ import java.net.http.HttpResponse;
 class BookRESTRequest {
 
     @JsonIgnoreProperties(ignoreUnknown = true)
-    private static void getJSON() {
+    public static HttpResponse<String> getJSON() {
         try {
             String restUrl =
                     "http://localhost:8080/api/books";
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
                     .GET()
-                    .header("accept", "application/json")
+                    .header("Accept", "application/json")
                     .uri(URI.create(restUrl))
                     .build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             ObjectMapper objectMapper = new ObjectMapper();
             objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            System.out.println(response.body());
+            return response;
         } catch (Throwable e) {
             System.out.println("Error: " + e);
             e.printStackTrace();
+            return null;
         }
     }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
-    private static void addJSON(Book book) {
+    public static HttpResponse<String> addJSON(Book book) {
         try {
             String restUrl =
                     "http://localhost:8080/api/books";
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
                     .POST(HttpRequest.BodyPublishers.ofString(book.toString()))
-                    .header("accept", "application/json")
+                    .header("Content-Type", "application/json")
                     .uri(URI.create(restUrl))
                     .build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
+            return response;
         } catch (Throwable e) {
             System.out.println("Error: " + e);
             e.printStackTrace();
+            return null;
         }
     }
 }
--- client/src/test/java/com/project/client/RESTapiclients/BookRESTRequestTest.java
+++ client/src/test/java/com/project/client/RESTapiclients/BookRESTRequestTest.java
@@ -1,26 +1,23 @@
 package com.project.client.RESTapiclients;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.net.http.HttpResponse;
-
-import static org.junit.jupiter.api.Assertions.fail;
 
 class BookRESTRequestTest {
     @Test
     void ensureThatBookAPICallReturnStatusCode200() throws Exception {
-        String url = "http://localhost:8080/api/books";
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .header("accept", "application/json")
-                .uri(URI.create(url))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = BookRESTRequest.getJSON();
+        assert response != null;
         assertEquals(200,response.statusCode());
+    }
+    @Test
+    void ensureThatBookAPICallReturnAGoodJson() throws Exception {
+        HttpResponse<String> response = BookRESTRequest.getJSON();
+        ObjectMapper jsonTest_2 = new ObjectMapper();
+        assertNotEquals(null,jsonTest_2.readTree(response.body()));
     }
 }
