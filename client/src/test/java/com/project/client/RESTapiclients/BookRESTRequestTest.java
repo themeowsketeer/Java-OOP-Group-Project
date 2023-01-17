a1f83eb7f6fe516d132d83706208e899dd27ec69
Tom Lawton <vnminhduy0750@outlook.com>
2023-01-17 17:06:32 +0700

    Improvised further the testcases

--- client/src/test/java/com/project/client/RESTapiclients/BookRESTRequestTest.java
+++ client/src/test/java/com/project/client/RESTapiclients/BookRESTRequestTest.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.client.object.Author;
 import com.project.client.object.Book;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import java.net.http.HttpResponse;
@@ -16,9 +18,63 @@ import static org.junit.jupiter.api.Assertions.*;
 class BookRESTRequestTest {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    Set<Author> authorSet = new HashSet<>();
+    Date now = new Date();
+
+    private final Book sampleBook = new Book("3", "Wraith Arc", authorSet, 2013L, 5, 2, now);
+
+//    @Test
+//    void ensureThatServerIsRunning() {
+//        ServerApplication();
+//    }
+
     @Test
-    void ensureThatBookAPICallReturnStatusCode200() {
+    void ensureThatGetBookAPICallReturnSuccessful() throws JSONException {
         HttpResponse<String> responseTest = BookRESTRequest.getBookByID(1);
+        if (responseTest == null) {
+            fail("Response returns none!");
+        } else {
+            assertEquals(200, responseTest.statusCode());
+            JSONObject actual = new JSONObject();
+            actual.put("id", 3);
+            actual.put("name", "Wraith Arc");
+            actual.put("edition", 5);
+            actual.put("quantity", 2);
+            actual.put("releasedYear", 2013L);
+            actual.put("placedAt", now);
+            System.out.println(responseTest.body());
+        }
+    }
+
+    @Test
+    void ensureThatPOSTMethodSent() {
+        authorSet.add(new Author(4, "Inu Curry"));
+
+        HttpResponse<String> responseTest = BookRESTRequest.addNewBook(sampleBook);
+        if (responseTest == null) {
+            fail("Response returns none!");
+        } else {
+            assertEquals(201, responseTest.statusCode());
+            System.out.println(responseTest.body());
+        }
+    }
+
+    @Test
+    void ensureThatBookAPICallReturnAGoodJson() throws JsonProcessingException {
+        HttpResponse<String> responseTest = BookRESTRequest.getBookByID(3);
+        if (responseTest == null) {
+            fail("Response returns none!");
+        } else {
+            assertNotEquals(null, objectMapper.readTree(responseTest.body()));
+        }
+    }
+
+    @Test
+    void ensureThatPUTMethodSent() {
+        sampleBook.setName("Madoka Magica");
+        authorSet.add(new Author(5, "Gen Doka"));
+
+        HttpResponse<String> responseTest = BookRESTRequest.updateBookByID(3,sampleBook);
         if (responseTest == null) {
             fail("Response returns none!");
         } else {
@@ -26,43 +82,8 @@ class BookRESTRequestTest {
             System.out.println(responseTest.body());
         }
     }
-
     @Test
-    void ensureThatBookAPICallReturnAGoodJson() throws JsonProcessingException {
-        HttpResponse<String> responseTest = BookRESTRequest.getBookByID(2);
-        if (responseTest == null) {
-            fail("Response returns none!");
-        } else {
-            assertNotEquals(null, objectMapper.readTree(responseTest.body()));
-        }
-    }
-    @Test
-    void ensureThatPOSTandPUTmethodSent() {
-        Set<Author> authorSet = new HashSet<>();
-        Date now = new Date();
-        authorSet.add(new Author(4,"Inu Curry"));
-        Book sampleBook = new Book("3","Wraith Arc",authorSet,2013L,5,2,now);
-
-        HttpResponse<String> responseTest_1 = BookRESTRequest.addnewBook(sampleBook);
-        if (responseTest_1 == null) {
-            fail("Response returns none!");
-        } else {
-            assertEquals(201, responseTest_1.statusCode());
-            System.out.println(responseTest_1.body());
-        }
-
-        sampleBook.setName("Madoka Magica");
-
-        HttpResponse<String> responseTest_2 = BookRESTRequest.updateBookByID(3,sampleBook);
-        if (responseTest_2 == null) {
-            fail("Response returns none!");
-        } else {
-            assertEquals(200, responseTest_2.statusCode());
-            System.out.println(responseTest_2.body());
-        }
-    }
-    @Test
-    void ensureThatDELETEmethodSent() {
+    void ensureThatDELETEMethodSent() {
         HttpResponse<String> responseTest = BookRESTRequest.deleteBookByID(3);
         if (responseTest == null) {
             fail("Response returns none!");
